c7bb291ca36c272514639e99b82767d4007faa51
Documenter.jl <documenter@juliadocs.github.io>
2023-01-10 21:26:45 +0000

    build based on d66f7d0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RelativisticRayTracing","category":"page"},{"location":"#RelativisticRayTracing","page":"Home","title":"RelativisticRayTracing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RelativisticRayTracing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RelativisticRayTracing]","category":"page"}]
+}
